97b68f04edea06f7b39a579f173c9c35f136ef58
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-08-04 16:08:20 +0100

    Initial files

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
             product: 'Socks',
             brand: 'Vue Mastery',
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
         }
     },
@@ -17,21 +17,18 @@ const app = Vue.createApp({
             this.cart += 1
         },
         updateVariant(index) {
-            this.selectedVariant = index;
+            this.selectedVariant = index
         }
     },
     computed: {
         title() {
-            return `${this.brand} ${this.product}`;
+            return this.brand + ' ' + this.product
         },
         image() {
-           return this.variants[this.selectedVariant].image;
+            return this.variants[this.selectedVariant].image
         },
         inStock() {
-           return this.variants[this.selectedVariant].quantity;
+            return this.variants[this.selectedVariant].quantity
         },
-        onSale() {
-           return this.variants[this.selectedVariant].onSale;
-        }
     }
 })
