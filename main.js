8bc83a1a308f7991242794254b7d36f61b61e16c
Mark Phoenix <mark@pixelsmiths.co.uk>
2022-08-04 15:54:33 +0100

    On sale computed

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ const app = Vue.createApp({
             selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onSale: true },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onSale: false },
             ]
         }
     },
@@ -29,6 +29,9 @@ const app = Vue.createApp({
         },
         inStock() {
            return this.variants[this.selectedVariant].quantity;
+        },
+        onSale() {
+           return this.variants[this.selectedVariant].onSale;
         }
     }
 })
